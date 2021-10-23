8c53f11905f7833143fbca91eeb6c7e8e3a47692
IlhamTahir <yantaq@bilig.biz>
2021-10-23 09:34:46 +0800

    Initial Commit

---
+++ uploadCloudFunction.bat
@@ -0,0 +1,1 @@
+"C:\Program Files (x86)\Tencent\微信web开发者工具\cli.bat" cloud functions deploy --e cloud1-4g2sqww73b1fdf3f --n quickstartFunctions --r --project "C:\Users\Administrator\WeChatProjects\tesla-cloud" --report_first --report
