1f3ae5258a62ba187264c93a47b4ba904faa8ba8
IlhamTahir <yantaq@bilig.biz>
2021-11-30 23:44:05 +0800

    feat: 活动根据位置切换信息筛选及活动列表获取

--- miniprogram/pages/activity/index.js
+++ miniprogram/pages/activity/index.js
@@ -1,4 +1,6 @@
 // pages/activity/index.js
+const citySelector = requirePlugin('citySelector');
+import {getCurrentLocation } from '../../utils/location.js'
 Page({
 
     /**
@@ -10,14 +12,59 @@ Page({
             '../../images/tesla.jpeg',
             '../../images/tesla.jpeg'
         ],
-        current: 0
+        current: 0,
+        currentCity: '',
+        activities: [],
+        recommendActivities: []
     },
 
     /**
      * 生命周期函数--监听页面加载
      */
     onLoad: function (options) {
+        const { city }  = getCurrentLocation()
+        this.setData({
+            currentCity: city
+        })
 
+        this.db = wx.cloud.database()
+    },
+
+    _loadCurrentCityLatestActivities() {
+        this.db.collection('activity').orderBy('created_time', 'desc').where({city: this.data.currentCity}).get().then(res=>{
+            let activities = []
+            res.data.forEach((item)=>{
+                console.log(item)
+                item.status = this._getActivityStatus(item)
+                item['start_time'] = this.formatDate(item['start_time'])
+                item['end_time'] = this.formatDate(item['end_time'])
+            
+                activities.push(item)
+            })
+
+            this.setData({
+                activities
+            })
+        })
+    },
+
+    _getActivityStatus(activity) {
+        const now = new Date()
+        return (activity['end_time'] <= now  && activity['start_time'] >= now)? 'signing-up' : 'signing-end'
+    },
+
+    formatDate(date) {
+        console.log(date)
+        console.log(date.getMonth())
+        return `${date.getMonth()}月${date.getDate()}日 ${date.getHours()}:${date.getMinutes()}`
+   },
+    _loadCurrentCityRecommendActivities() {
+        this.db.collection('activity').orderBy('created_time', 'desc').where({city: this.data.currentCity, recommend: true}).get().then(res=>{
+            console.log(res)
+            this.setData({
+                recommendActivities: res.data
+            })
+        })
     },
 
     /**
@@ -31,7 +78,15 @@ Page({
      * 生命周期函数--监听页面显示
      */
     onShow: function () {
+        const selectedCity = citySelector.getCity(); // 选择城市后返回城市信息对象，若未选择返回null
+        if(selectedCity) {
+            this.setData({
+                currentCity: selectedCity.fullname
+            })
+        }
 
+        this._loadCurrentCityLatestActivities()
+        this._loadCurrentCityRecommendActivities()
     },
 
     /**
@@ -44,6 +99,14 @@ Page({
     onSwiperChange(e) {
         const {current} = e.detail
         this.setData({current})
+    },
+
+    onTapSwitchLocation() {
+        const key = 'XGBBZ-HYV6W-MJ3RA-OHQCA-FGNYS-2WFW4'; // 使用在腾讯位置服务申请的key
+        const referer = '程序猿依力'; // 调用插件的app的名称
+        wx.navigateTo({
+        url: `plugin://citySelector/index?key=${key}&referer=${referer}`,
+        })
     },
 
     /**
