86d0620c27610dab677588e7fef74bae7926b5da
IlhamTahir <yantaq@bilig.biz>
2021-12-09 17:50:49 +0800

    feat: 完成day31

--- miniprogram/components/tesla-input/index.js
+++ miniprogram/components/tesla-input/index.js
@@ -3,6 +3,9 @@ Component({
     /**
      * 组件的属性列表
      */
+    options: {
+        multipleSlots: true // 在组件定义时的选项中启用多slot支持
+      },
     properties: {
         required: {
             type: Boolean,
